f5f0546170c8bece96c9d7e402a982f91ce08e87
Caleb Anderson <ande4905@gmail.com>
2021-12-31 11:13:03 -0600

    updated iptables script

--- script/iptables.sh
+++ script/iptables.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# disable firewalld
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl mask firewalld
+
+# clear all iptables
+iptables -X
+iptables -F
+# set default deny
+iptables -P FORWARD DROP
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+
+# setup my services
 iptables -A INPUT -p tcp -m multiport --dports 25,80,110,143 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --sports 25,80,110,143 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
