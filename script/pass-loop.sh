87dae89acf10b0c5977de4b484898a8c03c9b5c0
Caleb Anderson <ande4905@gmail.com>
2021-12-31 10:20:49 -0600

    added set shell to nologin

--- script/pass-loop.sh
+++ script/pass-loop.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 users=$(getent passwd {1000..6000} | cut -d ":" -f 1)
 for user in $users; do
+  # give random 8 char password
   pass=$(openssl rand -base64 6)
   echo "$user:$pass" | chpasswd
   echo "$user:$pass" >> fedora_users.csv
+  # set shell to nologin
+  /usr/sbin/usermod -s /usr/sbin/nologin $user
 done
